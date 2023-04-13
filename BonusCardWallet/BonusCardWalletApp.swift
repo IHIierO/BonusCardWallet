9b702aa96b97df61eebe0558dea0aaa24744b408
Artem <t-e-m-a_@mail.ru>
2023-04-13 10:33:22 +0300

    Initial Commit

---
+++ BonusCardWallet/BonusCardWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  BonusCardWalletApp.swift
+//  BonusCardWallet
+//
+//  Created by Artem Vorobev on 13.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct BonusCardWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
